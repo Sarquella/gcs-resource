1c273e7019738817752f8b0b8b8bbc6272cd95c5
Adrià Sarquella <adria@sarquella.email>
2019-11-15 00:24:16 +0100

    Added func in commons to retrieve DynamicPathValue from file

--- commons.go
+++ commons.go
@@ -1,8 +1,23 @@
 package gcsresource
 
 import (
+	"io/ioutil"
 	"strings"
 )
+
+func DynamicPathValue(file string) (string, error) {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return "", err
+	}
+
+	value := string(data)
+
+	value = strings.Replace(value, "\r\n", "\n", -1) //For Windows support
+	value = strings.Split(value, "\n")[0] //Only takes first line
+
+	return value, nil
+}
 
 func DynamicPath(path, replaceKey, replaceValue string) string{
 	return strings.Replace(path, replaceKey, replaceValue, -1)
